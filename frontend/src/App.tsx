82128ca021145508ed1149911386322d11a2da24
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-04-26 14:38:02 -0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello wolrd</h1>
+    <GlobalStyles />
+  </>
+);
+
+export default App;

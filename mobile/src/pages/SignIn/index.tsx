9b29fc7bec8af073e53c15fa9e82f0111b97bb44
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-04-29 22:06:41 -0300

    Mobile: signin screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,16 +1,60 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, KeyboardAvoidingView, ScrollView, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import {Container, Title} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import {
+  Container,
+  Title,
+  ForgotPassowordButton,
+  ForgotPassowordButtonText,
+  CreateAccountButtonText,
+  CreateAccountButton,
+} from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu logon</Title>
-    </Container>
+    <>
+      <KeyboardAvoidingView style={{flex: 1}} enabled>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{flex: 1}}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log();
+              }}>
+              Entrar
+            </Button>
+            <ForgotPassowordButton
+              onPress={() => {
+                console.log();
+              }}>
+              <ForgotPassowordButtonText>
+                Esqueci minha senha?
+              </ForgotPassowordButtonText>
+            </ForgotPassowordButton>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
+        onPress={() => {
+          console.log();
+        }}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
 import {Platform} from 'react-native';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 200 : 40}px;
+  padding: 0 30px ${Platform.OS === 'android' ? 5 : 40}px;
 `;
 
 export const Title = styled.Text`
@@ -33,10 +34,11 @@ export const CreateAccountButton = styled.TouchableOpacity`
   background: #312e38;
   border-top-width: 1px;
   border-color: #232129;
-  padding: 16px 0px;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
   align-items: center;
   flex-direction: row;
   justify-content: center;
+  display: flex;
 `;
 
 export const CreateAccountButtonText = styled.Text`

c76e51ab7e5905941be7da4937896691c979ed5d
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-05-07 14:00:35 -0300

    Backend: DDD

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import uploadConfig from '../config/upload';
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
-import isAuthenticated from '../middlewares/isAtuthenticated';
+import isAuthenticated from '@modules/users/infra/http/middlewares/isAtuthenticated';
+import uploadConfig from '@config/upload';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);

b1afbc2f3b302e5d944d881ad7eb66b50cf85576
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-05-10 18:48:36 -0300

    Backend: Controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,37 +1,13 @@
 import { Router } from 'express';
-// import { getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
-
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 import isAuthenticated from '@modules/users/infra/http/middlewares/isAtuthenticated';
-// import Appointment from '../../typeorm/entities/Appointment';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(isAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointmentsRepository = new AppointmentsRepository();
-
-//   // const appointmentsRepository = getCustomRepository(Appointment);
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRouter.post('/', async (request, response) => {
-  const { date, provider_id } = request.body;
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json({ appointment });
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;

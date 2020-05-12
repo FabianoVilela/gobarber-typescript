cd8c68b3f95e847c1a718eaf4f6e3fe8b34dc599
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-05-11 22:01:45 -0300

    Backend: providers and DTOs

---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,20 @@
+import { injectable, inject } from 'tsyringe';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+// import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(): Promise<void> {}
+}
